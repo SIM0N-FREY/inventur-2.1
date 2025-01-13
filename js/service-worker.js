fd2991fa3b4db051ac9e7b55b017306562b757f1
SIM0N-FREY <frey.kunst@gmail.com>
2025-01-13 14:33:44 +0100

    Create service-worker.js

---
+++ js/service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+const CACHE_NAME = 'inventur-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
